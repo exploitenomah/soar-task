a719eb78fd24f3cd4d7e95ac4af89acdad8dc215
exploitenomah <iksploiting@gmail.com>
2025-03-18 14:38:40 +0100

    fix settings layout

--- src/layouts/SettingsLayout.tsx
+++ src/layouts/SettingsLayout.tsx
@@ -5,7 +5,7 @@ import { settingsLinks } from "../components/SideNav/navlinks"
 export default function SettingsLayout() {
   return (
     <>
-      <div className="bg-white px-5 pt-[1.4rem] md:p-[1.875rem] rounded-[15px] md:rounded-[25px]">
+      <div className="bg-white px-5 pt-[1.4rem] md:p-[1.875rem] rounded-[15px] md:rounded-[25px] mx-[1.5625rem]">
         <TabNavigation tabLinks={settingsLinks} />
         <div className="px-5 md:px-[1.875rem] pt-[2.8125rem] md:pt-[2.5625rem] min-h-[65dvh]">
           <Outlet />
