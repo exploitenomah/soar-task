b76c595b30072dfd0124e2fbd984e6cb013ff220
exploitenomah <iksploiting@gmail.com>
2025-03-18 13:04:36 +0100

    create settings page layout

--- src/layouts/SettingsLayout.tsx
+++ src/layouts/SettingsLayout.tsx
@@ -1,12 +1,16 @@
-import { Outlet } from "react-router";
-import TabNavigation from "../components/reusable/TabNavigation/TabNavigation";
-import { settingsLinks } from "../components/SideNav/navlinks";
+import { Outlet } from "react-router"
+import TabNavigation from "../components/reusable/TabNavigation/TabNavigation"
+import { settingsLinks } from "../components/SideNav/navlinks"
 
 export default function SettingsLayout() {
   return (
     <>
-      <TabNavigation tabLinks={settingsLinks} />
-      <Outlet />
+      <div className="bg-white px-5 pt-[1.4rem] md:p-[1.875rem] rounded-[15px] md:rounded-[25px]">
+        <TabNavigation tabLinks={settingsLinks} />
+        <div className="px-5 md:px-[1.875rem] pt-[2.8125rem] md:pt-[2.5625rem] min-h-[65dvh]">
+          <Outlet />
+        </div>
+      </div>
     </>
   )
 }
