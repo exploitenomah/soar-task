8a69bea4e9192fe34d6fe1abe78183dd7e08a507
exploitenomah <iksploiting@gmail.com>
2025-03-19 18:45:24 +0100

    refactor to allow error text display

--- src/components/reusable/AppInput.tsx
+++ src/components/reusable/AppInput.tsx
@@ -4,12 +4,16 @@ import DownChevronIcon from "../../assets/icons/DownChevronIcon"
 const appInputClassNames =
   "w-full border border-solid border-[#DFEAF2] rounded-[0.625rem] lg:rounded-2xl py-3 lg:py-4 px-5 placeholder:text-xs md:placeholder:text-[0.94rem] placeholder:text-primary-dark-blue placeholder:font-normal  text-xs md:text-[0.94rem] text-primary-dark-blue font-normal focus:outline-primary-dark/50 leading-[100%]"
 
+const errorInputClassNames = `${appInputClassNames} border-red-500 focus:outline-red-500/50`
+
 export default function AppInput({
   label,
   inputProps,
+  error,
 }: {
   inputProps: InputHTMLAttributes<HTMLInputElement>
   label: ReactNode | ReactNode[]
+  error?: string | boolean
 }) {
   return (
     <>
@@ -17,19 +21,31 @@ export default function AppInput({
         <span className="leading-[100%] text-[0.8125rem] md:text-base text-primary-dark">
           {label}
         </span>
-        <div className="relative w-full">
-          {inputProps.type === "date" || inputProps.type === "datetime-local" ? (
-            <DateInput inputProps={inputProps} />
-          ) : (
-            <input {...inputProps} className={appInputClassNames} />
-          )}
+        <div>
+          <div className="relative w-full">
+            {inputProps.type === "date" || inputProps.type === "datetime-local" ? (
+              <DateInput inputProps={inputProps} error={error} />
+            ) : (
+              <input
+                {...inputProps}
+                className={error ? errorInputClassNames : appInputClassNames}
+              />
+            )}
+          </div>
+          {error && <span className="text-red-500 text-xs md:text-xs mt-[2px]">{error}</span>}
         </div>
       </label>
     </>
   )
 }
 
-function DateInput({ inputProps }: { inputProps: InputHTMLAttributes<HTMLInputElement> }) {
+function DateInput({
+  inputProps,
+  error,
+}: {
+  inputProps: InputHTMLAttributes<HTMLInputElement>
+  error?: string | boolean
+}) {
   const inputRef = useRef<HTMLInputElement | null>(null)
 
   const showInputPicker = useCallback(() => inputRef.current?.showPicker(), [])
@@ -37,7 +53,11 @@ function DateInput({ inputProps }: { inputProps: InputHTMLAttributes<HTMLInputEl
   if (inputProps.type !== "date" && inputProps.type !== "datetime-local") return null
   return (
     <div className="w-full">
-      <input ref={inputRef} {...inputProps} className={appInputClassNames} />
+      <input
+        ref={inputRef}
+        {...inputProps}
+        className={error ? errorInputClassNames : appInputClassNames}
+      />
       <span
         onClick={showInputPicker}
         className="rounded-tl-2xl rounded-bl-2xl text-[#718EBF] absolute inset-y-[5%] left-0.5 pl-5 flex justify-start items-center bg-white w-[90%] h-[90%] text-xs md:text-[0.94rem]"
