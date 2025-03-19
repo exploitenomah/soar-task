f6aabd682f2bf6b417a9c67eb516f474afdf732d
exploitenomah <iksploiting@gmail.com>
2025-03-19 09:32:56 +0100

    refactor styling

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,32 +5,33 @@ import QuickTransferSection from "../components/dashboard/QuickTransferSection"
 import RecentTransactionsSection from "../components/dashboard/RecentTransactionsSection"
 import WeeklyActivitySection from "../components/dashboard/WeeklyActivitySection"
 
-
 export default function DashboardPage() {
   return (
     <div className="pl-[1.5625rem] xl:p-0">
-      <main className="flex flex-col gap-[22px] xl:grid xl:grid-cols-3 xl:gap-x-6 xl:gap-y-[1.875rem] xl:grid-rows-2">
-        <section className="col-start-1 col-span-2 row-start-1 row-end-2">
+      <div className="flex flex-col xl:flex-row gap-[22px] xl:gap-6 mb-[22px] xl:mb-6">
+        <section className="flex-grow">
           <CardsSection />
         </section>
-        <section className="col-start-3 col-end-4 row-start-1 row-end-2 pr-[1.5625rem] xl:pr-0">
+        <section className="pr-[1.5625rem] xl:pr-0 xl:w-[380px]">
           <RecentTransactionsSection />
         </section>
-        <section className="col-start-1 col-span-2 row-start-2 row-end-3">
+      </div>
+      <div className="flex flex-col xl:flex-row gap-[22px] xl:gap-6 mb-[22px] xl:mb-6">
+        <section className="flex-grow pr-[1.5625rem] xl:pr-0">
           <WeeklyActivitySection />
         </section>
-        <section className="col-start-3 col-end-4 row-start-2 row-end-3">
+        <section className="xl:w-[380px] pr-[1.5625rem] xl:pr-0 ">
           <ExpenseStatisticsSection />
         </section>
-      </main>
-      <section className="flex flex-col gap-[22px] xl:grid xl:grid-cols-12 xl:gap-x-6 xl:gap-y-[1.875rem] mt-[22px] xl:mt-6  pr-[1.5625rem] xl:pr-0">
-        <section className="col-start-1 col-span-5">
+      </div>
+      <div className="flex flex-col xl:flex-row gap-[22px] xl:gap-6 pr-[1.5625rem] xl:pr-0">
+        <section className="xl:w-[480px]">
           <QuickTransferSection />
         </section>
-        <section className="col-start-6 col-end-13 row-start-1 row-end-2">
+        <section className="flex-grow">
           <BalanceHistorySection />
         </section>
-      </section>
+      </div>
     </div>
   )
 }
