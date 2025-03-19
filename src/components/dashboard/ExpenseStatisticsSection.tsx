9ca7f61dd6044b9f8de94bf4a3744508b49d17db
exploitenomah <iksploiting@gmail.com>
2025-03-19 11:17:03 +0100

    create expenses statistics section

--- src/components/dashboard/ExpenseStatisticsSection.tsx
+++ src/components/dashboard/ExpenseStatisticsSection.tsx
@@ -1,10 +1,15 @@
-import SectionHeading from "../reusable/SectionHeading";
+import SectionHeading from "../reusable/SectionHeading"
+import PieChart from "../reusable/Charts/PieChart"
 
-
-export default function ExpenseStatisticsSection(){
+export default function ExpenseStatisticsSection() {
   return (
-    <div>
+    <div className="flex flex-col gap-y-3 lg:gap-[18px]">
       <SectionHeading>Expense Statistics</SectionHeading>
+      <div className="bg-white rounded-[25px] p-6">
+        <div className="max-w-[280px] sm:max-w-[320px] mx-auto translate-x-[-10px]">
+          <PieChart data={[]} percentageKey={"percentage"} textKey={"category"} />
+        </div>
+      </div>
     </div>
   )
 }
