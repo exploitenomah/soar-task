988d35d6c9a20de602dbff098c49dc4766706ee5
exploitenomah <iksploiting@gmail.com>
2025-03-19 18:46:22 +0100

    create user slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,11 @@
-
-
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit"
 import uiReducer from "./slices/ui.slice"
+import userReducer from "./slices/user.slice"
 
 const store = configureStore({
   reducer: {
-    ui: uiReducer
+    ui: uiReducer,
+    user: userReducer,
   },
 })
 
@@ -13,4 +13,3 @@ export default store
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export type AppStore = typeof store
-
