1a96bb4d71f5ebfd7ebaea3f250725fa4407e90d
exploitenomah <iksploiting@gmail.com>
2025-03-20 10:45:50 +0100

    fix spacing

--- src/components/dashboard/ExpenseStatisticsSection.tsx
+++ src/components/dashboard/ExpenseStatisticsSection.tsx
@@ -8,7 +8,7 @@ export default function ExpenseStatisticsSection() {
   return (
     <div className="flex flex-col gap-y-3 lg:gap-[18px] h-full">
       <SectionHeading>Expense Statistics</SectionHeading>
-      <div className="bg-white rounded-[25px] p-6">
+      <div className="bg-white rounded-[25px] p-6 h-full">
         <div className="max-w-[280px] sm:max-w-[320px] mx-auto translate-x-[-10px]">
           <PieChart data={expensesStats} percentageKey={"percentage"} textKey={"category"} />
         </div>
