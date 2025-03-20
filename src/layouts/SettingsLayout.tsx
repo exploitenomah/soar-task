18469ea1ab2ef44ed322e6dcfaaf2df0f7988ac7
exploitenomah <iksploiting@gmail.com>
2025-03-20 07:22:30 +0100

    add page animation to settings layout

--- src/layouts/SettingsLayout.tsx
+++ src/layouts/SettingsLayout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router"
 import TabNavigation from "../components/reusable/TabNavigation/TabNavigation"
 import { settingsLinks } from "../components/SideNav/navlinks"
+import { PageWrapper } from "../components/reusable/PageWrapper"
 
 export default function SettingsLayout() {
   return (
     <>
       <div className="bg-white px-5 pt-[1.4rem] md:p-[1.875rem] rounded-[15px] md:rounded-[25px] mx-[1.5625rem]">
         <TabNavigation tabLinks={settingsLinks} />
-        <div className="px-5 md:px-[1.875rem] pt-[2.8125rem] md:pt-[2.5625rem] min-h-[65dvh]">
-          <Outlet />
-        </div>
+        <PageWrapper>
+          <div className="px-5 md:px-[1.875rem] pt-[2.8125rem] md:pt-[2.5625rem] min-h-[65dvh]">
+            <Outlet />
+          </div>
+        </PageWrapper>
       </div>
     </>
   )
