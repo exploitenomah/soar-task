221e9d70d254021e1596a6a6f74d39b0708cde81
exploitenomah <iksploiting@gmail.com>
2025-03-20 07:19:18 +0100

    adjust page stylings

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -25,12 +25,12 @@ export default function DashboardPage() {
   if (loadingDashboard) return <DashboardSkeleton />
   return (
     <Suspense fallback={<DashboardSkeleton />}>
-      <div className="pl-[1.5625rem] xl:p-0">
+      <div className="pl-[1.5625rem] xl:p-0 max-w-[2025px]">
         <div className="flex flex-col xl:grid xl:grid-cols-3 gap-[22px] xl:gap-6 mb-[22px] xl:mb-6">
           <section className="xl:col-start-1 xl:col-span-2">
             <CardsSection />
           </section>
-          <section className="pr-[1.5625rem] xl:pr-0 xl:max-w-[380px] xl:col-start-3 xl:col-end-4">
+          <section className="pr-[1.5625rem] xl:pr-0 xl:col-start-3 xl:col-end-4">
             <RecentTransactionsSection />
           </section>
         </div>
@@ -38,15 +38,15 @@ export default function DashboardPage() {
           <section className="flex-grow pr-[1.5625rem] xl:pr-0 xl:col-start-1 xl:col-span-2">
             <WeeklyActivitySection />
           </section>
-          <section className="xl:w-[380px] pr-[1.5625rem] xl:pr-0 xl:col-start-3 xl:col-end-4">
+          <section className="pr-[1.5625rem] xl:pr-0 xl:col-start-3 xl:col-end-4">
             <ExpenseStatisticsSection />
           </section>
         </div>
-        <div className="flex flex-col xl:flex-row gap-[22px] xl:gap-6 pr-[1.5625rem] xl:pr-0">
-          <section className="xl:w-[480px]">
+        <div className="flex flex-col xl:grid xl:grid-cols-10 gap-[22px] xl:gap-6 pr-[1.5625rem] xl:pr-0">
+          <section className="xl:col-start-1 xl:col-span-4">
             <QuickTransferSection />
           </section>
-          <section className="flex-grow">
+          <section className="xl:col-start-5 xl:col-span-6">
             <BalanceHistorySection />
           </section>
         </div>
